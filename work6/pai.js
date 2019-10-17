17d8756cfce32e363909bb8a477ab1d6fe9ce424
ynk666 <2506049119@qq.com>
2019-10-17 10:05:02 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,32 @@
+function calculate(){
+    let num = document.getElementById('num').Value
+    num =parseInt(num)&&Number(num)
+    if (!isNaN(num)){
+        document.getElementById('num').Value =pi(num)
+    }
+}
+function factorial(n){
+    let sum =1
+    for (let i=1; ;++i){
+        s
+    
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum =1
+    for(let i=1; ;sum ;++i){
+        s
+    }
+   return sum
+}
+function pi(num){
+    let sum =0
+    for (let i=0;i<=num;++i){
+        let dividend = factorial()
+        let divisor = oddFactorial( )
+        s.Value
+    }
+    return 
+}
+calculate()
