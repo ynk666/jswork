669274a346aa01395dffc4be4fe7379020d04432
ynk666 <2506049119@qq.com>
2019-12-12 22:10:46 +0800

    work16

--- work16/index2.js
+++ work16/index2.js
@@ -3,36 +3,31 @@ function animate(obj, option) {
     obj.timer = setInterval(function() {
         var flag = true;
         for (var k in option) {
-            var leader = parseInt(getStyle(obj, k)) || 0;
+            var leader = parseInt(getStyle(obj, k )) || 0;
             var target = option[k];
             var step = (target - leader) / 10;
             step = step > 0 ? Math.ceil(step) : Math.floor(step);
-            leader = step > 0 ? Math.ceil(step) : Math.floor(step);
             leader = leader + step;
-            obj.style[k] = leader + step;
             obj.style[k] = leader + 'px';
-            if (leader != target) {
+            if(leader != target) {
                 flag = false;
             }
-            }
-            if (flag) {
-                clearInterval(obj.timer);
-
-            }
+        }
+        if (flag) {
+            clearInterval(obj.timer);
+        }
         }, 15);
     }
-    function getStyle(obj,attr) {
-        if  (window.getComputedStyle) {
-            return  window.getComputedStyle(obj, null)[attr];
+    function getStyle(obj, attr) {
+        if (window.getComputedStyle) {
+            return window.getComputedStyle(obj, null)[attr];
         } else {
             return obj.currentStyle[attr];
         }
     }
     var obj = document.getElementById('box');
-    obj.onclick = function() {
-        animate(obj, {'left':200, 'top': 50});
+    obj.onclick = function (){
+        var left = parseInt(Math.random()*window.innerWidth);
+        var top = parseInt(Math.random()*window.innerHeight);
+        animate(obj, {'left':top, 'top':left});
     };
-
-        
-    
-        
