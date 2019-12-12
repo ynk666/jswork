a1e6ef8e75bd8f8baec1f62fb78f96261e263ff4
ynk666 <2506049119@qq.com>
2019-12-12 09:42:27 +0800

    work16

---
+++ work16/index1.js
@@ -0,0 +1,13 @@
+function animate(obj, option) {
+    clearInterval(obj,timer);
+    obj.timer = setInterval(function() {
+        var flag = true;
+        for (var k in option) {
+            var leader = parseInt(getStyle(obj, k)) || 0;
+            var target = option[k];
+
+        
+    
+        }
+    }
+}
